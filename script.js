a0243516cbb4666a4927f9fd73977f60573f4404
— NooR ShaikH <160132632+Noormajeed00@users.noreply.github.com>
2024-09-29 22:13:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const menuIcon = document.querySelector('.menu-icon');
+    const navUl = document.querySelector('nav ul');
+
+    menuIcon.addEventListener('click', () ={">"} {
+        navUl.classList.toggle('show')
+    });
+</script>
